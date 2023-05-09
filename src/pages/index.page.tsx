22ed5a827c9ddf15b01247a9182806b65cc01f3f
gregory6z <gregoryrag@gmail.com>
2023-05-09 10:26:37 +0200

    structuring the project

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,9 +1,5 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import { Heading } from "@ignite-ui/react"
+import Head from "next/head"
 
 export default function Home() {
   return (
@@ -15,7 +11,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1>ola</h1>
+        <Heading as="h1">Ola</Heading>
       </main>
     </>
   )
