49d8c59182ab8232fa7a07f8997e2bc89ff6f3be
gregory6z <gregoryrag@gmail.com>
2023-05-14 15:41:30 +0200

    create times intervals page

--- src/pages/register/times-intervals/index.page.tsx
+++ src/pages/register/times-intervals/index.page.tsx
@@ -1,68 +1,65 @@
-import { Button, Heading, MultiStep, Text } from "@ignite-ui/react"
-import { signIn, useSession } from "next-auth/react"
-import { useRouter } from "next/router"
-import { ArrowRight, Check } from "phosphor-react"
-// import { api } from "../../../lib/axios"
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from "@ignite-ui/react"
+import { ArrowRight } from "phosphor-react"
 import { Container, Header } from "../styles"
-import { AuthError, ConnectBox, ConnectItem } from "./styles"
 
-export default function ConnectCalendar() {
-  const session = useSession()
-  const router = useRouter()
+import {
+  IntervalBox,
+  IntervalContainer,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+} from "./styles"
 
-  const hasAuthError = !!router.query.error
-  const isSignedId = session.status === "authenticated"
-
-  async function handleConnectCalendar() {
-    await signIn("google")
-  }
-
-  console.log(session)
-
+export default function TimeIntervals() {
   return (
     <Container>
       <Header>
-        <Heading as="strong">Conecte sua agenda!</Heading>
+        <Heading as="strong">Quase lá</Heading>
         <Text>
-          Conecte o seu calendário para verificar automaticamente as horas
-          ocupadas e os novos eventos à medida em que são agendados.
+          Defina o intervalo de horário que você está disponível em cada dia da
+          semana.
         </Text>
 
-        <MultiStep size={4} currentStep={2} />
+        <MultiStep size={4} currentStep={3} />
       </Header>
 
-      <ConnectBox>
-        <ConnectItem>
-          <Text>Google Calendar</Text>
-          {isSignedId ? (
-            <Button size="sm" disabled>
-              Conectado
-              <Check />
-            </Button>
-          ) : (
-            <Button
-              variant="secondary"
-              size="sm"
-              onClick={handleConnectCalendar}
-            >
-              Conectar
-              <ArrowRight />
-            </Button>
-          )}
-        </ConnectItem>
+      <IntervalBox as="form">
+        <IntervalContainer>
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Segunda-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
 
-        {hasAuthError && (
-          <AuthError size="sm">
-            Falha ao se conectar ao Google, verifique se você habilitou as
-            permissões de acesso ao Google Calendar
-          </AuthError>
-        )}
+          <IntervalItem>
+            <IntervalDay>
+              <Checkbox />
+              <Text>Terça-feira</Text>
+            </IntervalDay>
+            <IntervalInputs>
+              <TextInput size="sm" type="time" step={60} />
+              <TextInput size="sm" type="time" step={60} />
+            </IntervalInputs>
+          </IntervalItem>
+        </IntervalContainer>
 
-        <Button type="submit" disabled={!isSignedId}>
+        <Button type="submit">
           Próximo passo
           <ArrowRight />
         </Button>
-      </ConnectBox>
+      </IntervalBox>
     </Container>
   )
 }
