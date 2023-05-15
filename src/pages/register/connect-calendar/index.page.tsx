94c6b44f9c80c0a0dfa2ba3b83d42d423a2fc95b
gregory6z <gregoryrag@gmail.com>
2023-05-15 18:17:45 +0200

    create update-profile

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -17,6 +17,10 @@ export default function ConnectCalendar() {
     await signIn("google")
   }
 
+  async function handleNavigateToNextStep() {
+    await router.push("/register/time-intervals")
+  }
+
   console.log(session)
 
   return (
@@ -58,7 +62,11 @@ export default function ConnectCalendar() {
           </AuthError>
         )}
 
-        <Button type="submit" disabled={!isSignedId}>
+        <Button
+          onClick={handleNavigateToNextStep}
+          type="submit"
+          disabled={!isSignedId}
+        >
           Próximo passo
           <ArrowRight />
         </Button>
